5ae3a6a4deb500db9b5f42550afd7ab057c1ef48
Austin <austin.wood1776@live.com>
2019-01-31 21:19:12 -0800

    Lab 2 Submission

--- js/app.js
+++ js/app.js
@@ -7,8 +7,13 @@ console.log(enjoy);
 if (enjoy.toLowerCase() === "yes"){
   alert("Exactly! It's a wonderful challenge!");
 }
+
+if (enjoy.toLowerCase() ==="no"){
+  alert("Nope, I really do enjoy it. What a challenge!")
+}
+
   else{
-    alert("Okay, good guess but I really do. I even started taking courses to learn it..."); 
+  alert("Hey, let's stick to yes/no")
 }
 
 var money = prompt("Okay so review, am I going into the coding field for the money?");
@@ -20,4 +25,59 @@ if(money.toLowerCase() === "no"){
 
 if(money.toLowerCase() === "yes"){
   alert("Yeah, you got me. I'm in it for the money too.");
+}
+  else{
+    alert("Hey, let's stick to yes/no")
+  }
+
+var animals = prompt("Do I prefer cats or dogs?");
+console.log(animals);
+
+if (animals.toLowerCase() === "dogs"){
+  alert("Yeah, thats right but I was supposed to prompt you with a yes or no question. Let's try again.");
+}
+
+if (animals.toLowerCase() ==="cats"){
+  alert("No, I prefer dogs. but I was supposed to ask a yes or no question so let's try again");
+}
+else{
+  alert("Hey, let's stick to yes/no")
+}
+
+var dogs = prompt("Do I like dogs?");
+console.log(dogs);
+
+if (dogs.toLowerCase() === "yes"){
+  alert("Thats right! Great guess!");
+}
+
+if (dogs.toLowerCase() === "no"){
+  alert( "Okay, I definitely  prefer dogs because they're the best.");
+}
+else{
+  alert("Hey, let's stick to yes/no")
+}
+
+var games = prompt("Do I play video games?");
+console.log(games);
+
+if (games.toLowerCase() ==="yes"){
+  alert("Yep! I love playing on my Xbox and laptop. I'm especially into Dead By Daylight right now.");
+}
+  else {
+  alert("I do! I've been playing for years!");
+  }
+
+var food = prompt("Is my favorite food carrots?");
+console.log(food);
+
+if (food.toLowerCase() === "yes"){
+  alert("Nope, It's pretty basic of me but I love pizza. :)");
+}
+
+if(food.toLowerCase() === "no"){
+  alert("That's right! It's actually pizza because I'm basic like that.");
+}
+else{
+  alert("Hey, let's stick to yes/no")
 }
